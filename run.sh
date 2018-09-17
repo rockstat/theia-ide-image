a142d41a0d07f233360b9d08dfdee9bca1df3602
Dmitry Rodin <madiedinro@gmail.com>
2018-09-17 18:11:23 +0300

    fix path

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cp -nR $HOME/bootstrap/.theia $HOME/project
+cp -nR /home/theia/bootstrap/.theia "$WORKSPACE_PATH"
 chown -R $RST_UID:$RST_GID "$WORKSPACE_PATH/.theia" "$USER_IMAGES_PATH"
 su-exec theia:theia yarn theia start /home/theia/project --hostname=0.0.0.0 --port=$PORT_THEIA
 
