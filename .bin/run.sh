d109c52e3df3a23d89fe6627920112b9aed92e34
Dmitry Rodin <madiedinro@gmail.com>
2018-09-25 10:41:14 +0300

    new env name

--- .bin/run.sh
+++ .bin/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git config --global user.email ${EMAIL:-"name@example.com"} \
-git config --global user.name ${REALNAME:-"Name Surname"} \
+git config --global user.name ${FULLNAME:-"Name Surname"} \
 cp -nR /home/theia/.bootstrap/.theia "$WORKSPACE_PATH"
 chown -R $RST_UID:$RST_GID "$WORKSPACE_PATH/.theia" "$WORKSPACE_PATH/my_images"
 su-exec theia:theia yarn theia start /home/theia/project --hostname=0.0.0.0 --port=$PORT_THEIA
